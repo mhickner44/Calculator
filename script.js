a08d69460eccead0813d7724bf7c9a0eb82fe085
mhickner44 <rmicu1903@gmail.com>
2023-01-21 11:41:23 -0500

    operators working correctly

--- script.js
+++ script.js
@@ -3,31 +3,63 @@ const textDisplay = document.querySelector(".textDisplay");
 const input = document.querySelectorAll(".input");
 const del = document.getElementById("del");
 const process = document.getElementById("process");
+const operators = document.querySelectorAll(".input.operator");
+const equalityOperator = document.getElementById("process");
 let displayValue;
 let equation;
+let count = 0;
+
+function operatorCounting() {
+    count++;
+    if (count == 2) {
+        processDisplay();
+        count = 1;
+    }
+}
 
 
 
 function displayText() {
-    textDisplay.textContent += this.textContent+"";
+    textDisplay.textContent += this.textContent + "";
 
 };
 
 function clearDisplay() {
     textDisplay.textContent = "";
+    count = 0;
 
 };
+
 function processDisplay() {
+    //for the operator  count
+   
+    let num1 = 0;
+    let num2 = 0;
     equation = textDisplay.textContent;
-    //turn them into the values we want now
-    let equationArr;
-    equationArr = equation.split(/(\W)/);
-    let num1 = parseInt(equationArr[0]);
-    let num2 = parseInt(equationArr[2]);
-    let operator = equationArr[1];
-    textDisplay.textContent = operate(num1, num2, operator);
+    const str = "You cannot divide by zero";
+    if (equation.includes(str)) {
+        //reset the text display 
+        return clearDisplay();
+    } else {
+        //turn them into the values we want now
+        let equationArr;
+        equationArr = equation.split(/(\W)/);
+        num1 = parseInt(equationArr[0]);
+        num2 = parseInt(equationArr[2]);
+        if (isNaN(num2)) {
+            num2 = num1;
+        }
+        let operator = equationArr[1];
+        textDisplay.textContent = operate(num1, num2, operator);
+    }
 };
+//looping all the operators to listen for presses
+operators.forEach(element => {
+    element.addEventListener("click", operatorCounting);
+});
 
+//equality operator count reset 
+equalityOperator.addEventListener("click", function () { count = 0; });
 //loop adding it to all elelemetns
 input.forEach(element => {
     element.addEventListener("click", displayText);
@@ -55,25 +87,28 @@ function multiply(num1, num2) {
 }
 
 function operate(num1, num2, operator) {
+    let answer = 0;
     switch (operator) {
         case "+":
             operator = "add";
             break;
-          case "-":
+        case "-":
             operator = "subtract";
             break;
-          case "*":
+        case "*":
             operator = "multiply";
             break;
-          case "/":
+        case "/":
             operator = "divide";
             break;
     }
-
-//how do I get the function to be the correct math function I want 
-    let answer = window[operator](num1, num2);
-    return Math.round(answer * 100) / 100;
+    answer = window[operator](num1, num2);
+    if (answer == "Infinity") {
+        answer = "You cannot divide by zero";
+    } else {
+        answer = Math.round(answer * 100) / 100
+    }
+    return answer;
 }
-// console.log(operate(5, 21, subtract));
 
 
